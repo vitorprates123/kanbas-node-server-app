50864dce17133864d024c4969886c7d009406efd
Vitor Prates <prates.v@northeastern.edu>
2024-11-22 22:20:43 -0500

    all

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
